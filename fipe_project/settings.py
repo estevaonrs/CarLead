b326721e8b78e30268c0e37ab27632e6d966e1ea
estevaonrs <tevonrs@gmail.com>
2024-03-07 19:45:37 -0300

    Script consertado

--- fipe_project/settings.py
+++ fipe_project/settings.py
@@ -92,12 +92,8 @@ WSGI_APPLICATION = 'fipe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cscnet',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'userdjango',
-        'PASSWORD': 'Teste1234@rt',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- script.py
+++ script.py
@@ -2,16 +2,17 @@ import os
 import sys
 import concurrent.futures
 import requests
-from datetime import datetime
+from django.db import models
+from django.db.utils import IntegrityError  # Importe a exceção de integridade
 
 # Configurar o ambiente do Django
-sys.path.append('C:\\Users\\iTrevo\\Desktop\\teste\\fipe_project')  # Ajuste para o caminho do seu projeto
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fipe_project.settings')
+sys.path.append('script/fipe_project/')  # Ajuste para o caminho do seu projeto
+os.environ['DJANGO_SETTINGS_MODULE'] = 'fipe_project.settings'
 import django
 django.setup()
 
 # Importar modelos do Django
-from fipe_app.models import FipeVehicleType, FipeBrand, FipeModel, FipeYear, FipeFuel, FipeCodeFipe, FipePrice
+from fipe_app.models import FipeVehicleType, FipeBrand, FipeModel, FipeYear, FipeFuel, FipeCodeFipe, FipePrice  # Substitua 'fipe_app' pelo nome do seu aplicativo Django
 
 # Função para fazer requisições à API Fipe
 def make_request(endpoint, params=None):
@@ -21,44 +22,57 @@ def make_request(endpoint, params=None):
     return response.json() if response.status_code == 200 else None
 
 # Função para obter informações Fipe e adicionar ao banco de dados
-def fetch_and_append_data(vehicle_type, brand, model, year):
-    # Adicionando filtragem para anos de modelo acima de 2009
-    if int(year["code"][:4]) > 2014:  # Ajuste aqui para incluir anos acima de 2009
-        vehicle_info = make_request(f'/{vehicle_type}/brands/{brand["code"]}/models/{model["code"]}/years/{year["code"]}')
-        
-        if vehicle_info:
-            print(f"Processando: {vehicle_info['brand']} - {vehicle_info['model']} - Ano: {vehicle_info['modelYear']}")  # Adicionado print para monitoramento
-            vehicle_type_instance, _ = FipeVehicleType.objects.get_or_create(vehicle_type="Carro")
-            brand_instance, _ = FipeBrand.objects.get_or_create(brand=vehicle_info["brand"], vehicle_type=vehicle_type_instance)
-            model_instance, _ = FipeModel.objects.get_or_create(model=vehicle_info["model"], brand=brand_instance)
-            year_instance, _ = FipeYear.objects.get_or_create(year=int(vehicle_info["modelYear"]), model=model_instance)
-            fuel_instance, _ = FipeFuel.objects.get_or_create(fuel=vehicle_info["fuel"], year=year_instance)
-            code_fipe_instance, _ = FipeCodeFipe.objects.get_or_create(code_fipe=vehicle_info["codeFipe"], model=model_instance)
-            FipePrice.objects.get_or_create(price=vehicle_info["price"], brand=brand_instance, model=model_instance, fuel=fuel_instance, year=year_instance)
+def fetch_and_append_data(vehicle_type, reference, brand, model, year):
+    # Obtendo informações Fipe específicas para o veículo
+    vehicle_info = make_request(f'/{vehicle_type}/brands/{brand["code"]}/models/{model["code"]}/years/{year["code"]}')
+    
+    if vehicle_info:
+        try:
+            # Obtendo ou criando os objetos relacionados
+            fipe_vehicle_type, created = FipeVehicleType.objects.get_or_create(vehicle_type=vehicle_type)
+            fipe_brand, created = FipeBrand.objects.get_or_create(brand=vehicle_info["brand"], vehicle_type=fipe_vehicle_type)
+            fipe_model, created = FipeModel.objects.get_or_create(model=vehicle_info["model"], brand=fipe_brand)
+            fipe_year, created = FipeYear.objects.get_or_create(year=int(vehicle_info["modelYear"]), model=fipe_model)
+            fipe_fuel, created = FipeFuel.objects.get_or_create(fuel=vehicle_info["fuel"], year=fipe_year)
+
+            # Adicionando dados ao banco de dados
+            FipePrice.objects.create(
+                price=vehicle_info["price"],
+                brand=fipe_brand,
+                model=fipe_model,
+                fuel=fipe_fuel,
+                year=fipe_year,
+            )
+            print(f"Saved: {vehicle_info['brand']} - {vehicle_info['model']} - Year: {vehicle_info['modelYear']} - Price: {vehicle_info['price']}")
+        except IntegrityError as e:
+            # Se houver uma violação de integridade (possível duplicata), imprima uma mensagem e ignore
+            print(f"Ignored duplicate entry: {vehicle_info['brand']} - {vehicle_info['model']} - Year: {vehicle_info['modelYear']} - Price: {vehicle_info['price']}")
+
 
 # Função para popular o banco de dados com informações da API Fipe
 def populate_database():
-    vehicle_types = ['cars']  # Continua limitado apenas a carros
+    # Populando o banco de dados com dados da API Fipe
+    vehicle_type = 'cars'  # Pegando apenas carros
+    reference = {'code': '202403', 'month': 'Março de 2024'}  # Especificando a referência do mês desejado (março de 2024)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
-
-        for vehicle_type in vehicle_types:
-            brands = make_request(f'/{vehicle_type}/brands')
-            if brands:
-                for brand in brands:
-                    models = make_request(f'/{vehicle_type}/brands/{brand["code"]}/models')
-                    if models:
-                        for model in models:
-                            years = make_request(f'/{vehicle_type}/brands/{brand["code"]}/models/{model["code"]}/years')
-                            if years:
-                                for year in years:
-                                    futures.append(
-                                        executor.submit(fetch_and_append_data, vehicle_type, brand, model, year)
-                                    )
-
-    # Aguardar a conclusão de todas as tarefas
-    concurrent.futures.wait(futures)
+    print(f'Processing {vehicle_type} - Reference: {reference["month"]}')
+    
+    # Obtendo marcas para o tipo de veículo
+    brands = make_request(f'/{vehicle_type}/brands')
+    if brands:
+        for brand in brands:
+            # Obtendo modelos para a marca
+            models = make_request(f'/{vehicle_type}/brands/{brand["code"]}/models')
+            if models:
+                for model in models:
+                    # Obtendo anos para o modelo
+                    years = make_request(f'/{vehicle_type}/brands/{brand["code"]}/models/{model["code"]}/years')
+                    if years:
+                        for year in years:
+                            # Verificando se o ano é maior que 2014
+                            if int(year["code"][:4]) > 2014:
+                                # Adicionando tarefa de busca e inserção
+                                fetch_and_append_data(vehicle_type, reference, brand, model, year)
 
 if __name__ == "__main__":
     populate_database()
